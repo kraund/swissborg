b46726d6f796cec9c0ddf80fc2930e0ac2c16aef
Vladyslav Stetsenko <vkraund@gmail.com>
2023-07-02 19:16:40 +0200

    minor changes to look better

--- tests/TC9.js
+++ tests/TC9.js
@@ -1,30 +1,30 @@
 describe('SwissBorg Task 1', function () {
 
-    ////enter any 2-digits integer to get correct calculation
+    ////enter any 2-digit integer to get correct calculation
     const numbersToTest = [10, 15, 20, 21, 22, 99]
 
-    numbersToTest.forEach((test_value) => {
-        it(`Testing the factorial of ${test_value} from the page`, async function () {
+    numbersToTest.forEach((testValue) => {
+        it(`Testing the factorial of ${testValue} from the page`, async function () {
             await browser.url('/')
 
-            const text_field = await browser.$(selector.text_field)
-            await text_field.addValue(test_value)
+            const textField = await browser.$(selector.textField)
+            await textField.addValue(testValue)
 
-            const calculate_button = await browser.$(selector.calculate_button)
-            await calculate_button.click()
+            const calculateButton = await browser.$(selector.calculateButton)
+            await calculateButton.click()
             await browser.waitUntil(
                 async () => {
-                    const messageObj = await browser.$(selector.message_body)
-                    const message = await messageObj.getText()
-                    return message.length > 0
+                    const messageObj = await browser.$(selector.messageBody)
+                    const messageText = await messageObj.getText()
+                    return messageText.length > 0
                 }, {
                     timeout: 2000
                 }
             )
 
-            const outputTextObj = await browser.$(selector.message_body)
+            const outputTextObj = await browser.$(selector.messageBody)
             const outputText = await outputTextObj.getText()
-            const expectedValue = await nxutils.factorialize(test_value)
+            const expectedValue = await nxutils.factorialize(testValue)
 
             expect(outputText).to.eq(expectedValue)
         });
